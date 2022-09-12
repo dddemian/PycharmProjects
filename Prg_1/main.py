44e04477cb011bbbb12fbcad164a1ee2e864574a
dddemian <dan.ddemian@gmail.com>
2022-09-12 22:59:17 +0300

    initial commit

---
+++ Prg_1/main.py
@@ -0,0 +1,6 @@
+name_patient = input("Nume pacient? ")
+age = input("Varsta? ")
+tip_pacient = input("Pacient existent? ")
+print(name_patient, age, tip_pacient)
+
+
---
+++ pythonProject/main.py
@@ -0,0 +1,19 @@
+# This is a sample Python script.
+print("Hello World")
+print("Hello World")
+print(1, 4.5, 3+5j)
+
+a = 1
+print(a)
+print(type(a))
+print("that's my dog")
+print("derv")
+
+
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
