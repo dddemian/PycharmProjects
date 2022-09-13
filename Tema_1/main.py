c56d660a6cb037d47fe66e2721377661b9b55964
dddemian <dan.ddemian@gmail.com>
2022-09-14 00:28:52 +0300

    initial commit

---
+++ Tema_1/main.py
@@ -0,0 +1,16 @@
+list_1 = [7, 8, 9, 2, 3, 1, 4, 10, 5, 6]
+#          0  1  2  3  4  5  6   7  8  9
+# print(len(list_1))
+print(list_1)
+
+list_2 = list_1.sort()
+print(list_2)
+
+list_3 = list_1.reverse()
+print(list_3)
+
+list_4 = list_1[1:]
+print(list_4)
+
+
+
